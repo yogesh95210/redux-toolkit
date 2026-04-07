48a15de16593f02e6ab1b1bdee64278b81de434c
Yogesh Sharma <webdeveloper.yogesh63@gmail.com>
2026-04-07 16:14:18 +0530

    #Cart UI First Commit

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,43 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import { decrement, increment, reset,incrementByAmount} from './features/counter/counterSlice'
-import { useState } from 'react'
 function App() {
-  const [amount,setAmount]= useState(0)
-  const count = useSelector((state)=> state.counter.value)
-  const dispatch = useDispatch()
-  
-  function handleIncrementClick(){ 
-    dispatch(increment())
-  }
-  function handleDecrementClick(){
-    dispatch(decrement())
-  }
-  function handleResetClick(){
-    dispatch(reset())
-  }
-  
-  function handleIncByAmountClick(){
-    dispatch(incrementByAmount(amount))
-  }
   
   return (
-    <div className='container'>
-     <button onClick={handleIncrementClick}>Increse</button>
-     <p>Count : {count}</p>
-     <button onClick={handleDecrementClick}>Decrese</button>
-     <button onClick={handleResetClick}>Reset</button>
-     <br/>
-     <br/>
-     <input
-      type='Number'
-      value={amount}
-      placeholder='Enter Increment Value'
-      onChange={(e)=> setAmount(e.target.value)}
-     />
-
-      <button onClick={handleIncByAmountClick}>Inc By Amount</button>
-    </div>
+     <div>
+     <h2> Cart UI</h2>
+     </div>
   )
 }
 
